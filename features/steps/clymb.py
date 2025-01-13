5336996a7dda84a2c2ed1a213e8510a30e4cf8a2
Abhishek Kumar <abhishekk@evincedev.com>
2025-01-13 12:29:38 +0530

    updated import

--- features/steps/clymb.py
+++ features/steps/clymb.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 import time
 import logging
